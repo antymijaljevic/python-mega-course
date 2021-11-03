0823fd9b1350e0a1ead9db4552f532e9032bca98
Anty Mijaljevic <antymijaljevic@gmail.com>
2021-11-03 10:59:32 +0000

    new

---
+++ app_1_web_mapping_with_python/126.py
@@ -0,0 +1,31 @@
+import folium
+import pandas
+ 
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+ 
+def color_alt(el):
+    if el > 2000:
+        return "red"
+    elif 1000 <= el <= 2000:
+        return "purple"
+    else:
+        return "green"
+ 
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+fg = folium.FeatureGroup(name = "My Map")
+ 
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+" m", fill_color = color_alt(el), color="gray", fill_opacity=0.7))
+
+
+fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+ 
+ 
+map.add_child(fg)
+map.save("map_color.html")
+
+# help(folium.CircleMarker)
+# help(folium.vector_layers.path_options)
